5b0b64d792439b22ed8b9611c9736c3fa59a19dc
subhaprasanna1415 <subhaprasanna1415@gmail.com>
2025-01-30 12:29:07 +0530

    Add files via upload

---
+++ selectedplan.js
@@ -0,0 +1,62 @@
+document.querySelector('.card-number-input').oninput = () =>{
+    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
+}
+
+document.querySelector('.card-holder-input').oninput = () =>{
+    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
+}
+
+document.querySelector('.month-input').oninput = () =>{
+    document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value;
+}
+
+document.querySelector('.year-input').oninput = () =>{
+    document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value;
+}
+
+document.querySelector('.cvv-input').onmouseenter = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+
+document.querySelector('.cvv-input').onmouseleave = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+}
+
+document.querySelector('.cvv-input').oninput = () =>{
+    document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
+}
+
+
+function work(){
+   let number=document.getElementById('number').value;
+   let number_status= false;
+  const  num=/^[0-9]+$/;
+
+  if(number==''){
+    document.getElementById("number").innerHTML="Only Digits";
+  }
+  else{
+    if(number.match(num)){
+        document.getElementById("n")
+    }
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+   let holder=document.getElementById('holder').value;
+   let cvv=document.getElementById('cvv').value;
+
+ 
+
+}
